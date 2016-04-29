695ed79e8e1722a83de98d8e7d4d97e7fdf0f239
ericli1989 <lishenglong1989@126.com>
2016-04-29 17:08:42 +0800

    spider学习仓库创建

---
+++ qzone.py
@@ -0,0 +1,26 @@
+import request
+import string
+
+class my_spider:
+   def __init__(self):
+      self.page = 1
+      self.pages = []
+      self.enable = False
+   # 获取以第一页面，由于qzone的加密方式有点复杂，不想将时间耗在这个
+   # 上面，第一个版本就不对加密方式进行处理，采取浏览器页面首次登陆
+   # 后获取相应的cookie值，手动写入相应配置中，然后执行脚本进行页面
+   # 抓取
+   
+   def GetFirstPage:
+
+
+
+   def start(self): 
+      self.enable = True
+      
+      
+      
+
+
+  spider = my_spider()
+  spider.start()	
